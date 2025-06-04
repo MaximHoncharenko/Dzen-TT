78beb56cd2a54274174ae1e89284de70e486f2f2
Max Honcharenko <157576499+MaximHoncharenko@users.noreply.github.com>
2025-06-04 15:52:18 +0300

    Implement code changes to enhance functionality and improve performance

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework_simplejwt',
     'channels',
+    'django_redis',
+    'django_extensions',  # для зручності розробки
+    
 ]
 
 MIDDLEWARE = [
